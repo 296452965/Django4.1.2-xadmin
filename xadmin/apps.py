bbf47a12034dcd98f6a17c3b066cd403c59c2d66
Yik chen <296452965@qq.com>
2022-11-22 15:38:02 +0800

    'init'

---
+++ xadmin/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+from django.core import checks
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+import xadmin
+
+
+class XAdminConfig(AppConfig):
+    """Simple AppConfig which does not do automatic discovery."""
+
+    name = 'xadmin'
+    verbose_name = _("Administration")
+
+    def ready(self):
+        self.module.autodiscover()
+        setattr(xadmin,'site',xadmin.site)
